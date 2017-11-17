61cffa407f9389c8e9f7d377e776852f5bd7d1c3
cfalino <31833404+cfalino@users.noreply.github.com>
2017-11-17 12:30:48 -0500

    Add files via upload

---
+++ Week 12/HW 2/script.js
@@ -0,0 +1,30 @@
+/* function changeColor() {
+	var newColor = document.getElementbyID("vote");
+	newColor.style.backgroundColor = "hotpink";
+ }
+*/
+
+
+/*function buttonClicked() {
+	console.log("it works");
+
+	//get firstname from form
+	var firstName = document.getElementbyID("firstName");
+	car firstNameToShow = firstName.value;
+
+	//get last name
+	var lastName = document.getElementbyID("lastName");
+	var lastNameToShow = lastName.value;
+
+	//display text on page - access empty paragraph and change text
+	var textToShow = document.getElementbyID("fullNameToShow");
+	textToShow.innerHTML
+} */  
+
+    function changeColor() {
+    	console.log("hello");
+
+    	var newColor = document.getElementById('change'); 
+        newColor.style.backgroundColor = "lightblue";
+        // return false;
+    }   
