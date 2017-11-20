7491f82643e59011954d9a61b02b23c6dc8aa3ae
cfalino <31833404+cfalino@users.noreply.github.com>
2017-11-20 16:58:09 -0500

    Add files via upload

---
+++ Week 13/script.js
@@ -0,0 +1,23 @@
+    function changeColor() {
+    	console.log("hello");
+
+    	var newColor = document.getElementById('change'); 
+        newColor.style.backgroundColor = "lightblue";
+        // return false;
+    } 
+    
+    function changeColor2() {
+    	console.log("hello!");
+
+    	var newColor = document.getElementById('change'); 
+        newColor.style.backgroundColor = "lightblue";
+        // return false;
+    } 
+
+    function changeColor3() {
+    	console.log("hello!!");
+
+    	var newColor = document.getElementById('change'); 
+        newColor.style.backgroundColor = "lightblue";
+        // return false;
+    }   
